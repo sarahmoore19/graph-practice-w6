92b1b01737e5d9c635d512e7723774c65a33c4f8
sarahmoore <sarahnmoore19@gmail.com>
2022-11-06 02:20:55 -0500

    implemented social network graph

--- social.js
+++ social.js
@@ -8,28 +8,62 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++;
+    this.users[this.currentID] = {"id": this.currentID, "name": name};
+    this.follows[this.currentID] = new Set([])
+    return this.currentID;
   }
 
   getUser(userID) {
-    // Your code here
+    if (userID in this.users) return this.users[userID];
+    return null;
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (userID1 in this.users && userID2 in this.users) {
+      this.follows[userID1].add(userID2);
+      return true;
+    }
+    return false
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID];
   }
 
+
   getFollowers(userID) {
-    // Your code here
+    let set = new Set([]);
+      for (let ele in this.follows) {
+        if (this.follows[ele].has(userID)) {
+          set.add(Number(ele));
+        }
+      }
+    return set;
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here
+    let queue = [[userID]];
+    let set = new Set([userID]);
+    let newArr = [];
+    while(queue.length) {
+      let currPath = queue.pop();
+      let currNode = currPath[currPath.length - 1];
+      if (currPath.length > 2 && currPath.length <= degrees + 2) {
+        newArr.push(currNode);
+      }
+      let follows = this.getFollows(currNode)
+      for (let ele of follows) {
+        if (!set.has(ele)) {
+          queue.unshift([...currPath, ele])
+          set.add(ele);
+        }
+      }
+    }
+    return newArr;
   }
 }
+
+socialNetwork = new SocialNetwork();
 
 module.exports = SocialNetwork;
